ffe433a4794bd1c3220cd11045d707d5d423078a
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-01-10 15:13:26 -0500

    Remove: padding on Contacts Holder

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 function Contacts({ contacts }: Props) {
   return (
-    <div className='flex flex-col h-full p-4 overflow-scroll'>
+    <div className='flex flex-col h-full overflow-scroll'>
       {contacts.map((contact) => (
         <Contact key={contact.id} contact={contact} />
       ))}
