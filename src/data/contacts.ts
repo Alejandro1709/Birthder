300e8266fbe1b5138c2d96c036e10c4c0504673a
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-01-10 14:36:00 -0500

    Add: Dummy contacts, Contact interface

---
+++ src/data/contacts.ts
@@ -0,0 +1,86 @@
+import type Contact from '../types/contact';
+
+const contacts: Contact[] = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'johndoe@mail.com',
+    phone: '555-555-5555',
+    avatar: 'https://randomuser.me/api/portraits',
+    birthday: '1990-01-01',
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+    email: 'jandedoe@mail.com',
+    phone: '222-222-2222',
+    avatar: 'https://randomuser.me/api/portraits',
+    birthday: '1990-01-01',
+  },
+  {
+    id: 3,
+    name: 'Jonathan Wexler',
+    email: 'wexler@gmail.com',
+    phone: '333-333-3333',
+    avatar: 'https://randomuser.me/api/portraits',
+    birthday: '1997-11-11',
+  },
+  {
+    id: 4,
+    name: 'Tyler Joseph',
+    email: 'tyler@twentyonepilots.com',
+    phone: '444-444-4444',
+    avatar: 'https://randomuser.me/api/portraits',
+    birthday: '1988-12-01',
+  },
+  {
+    id: 5,
+    name: 'Joshua Dun',
+    email: 'joshua@twentyonepilots.com',
+    phone: '555-555-5555',
+    avatar: 'https://randomuser.me/api/portraits',
+    birthday: '1988-12-01',
+  },
+  {
+    id: 6,
+    name: 'Jared Leto',
+    email: 'jared@leto.com',
+    phone: '666-666-6666',
+    avatar: 'https://randomuser.me/api/portraits',
+    birthday: '1971-12-26',
+  },
+  {
+    id: 7,
+    name: 'Mark Hoppus',
+    email: 'hoppus.m@hotmail.com',
+    phone: '777-777-7777',
+    avatar: 'https://randomuser.me/api/portraits',
+    birthday: '1972-01-15',
+  },
+  {
+    id: 8,
+    name: 'Travis Barker',
+    email: 'barker@woof.com',
+    phone: '888-888-8888',
+    avatar: 'https://randomuser.me/api/portraits',
+    birthday: '1975-11-14',
+  },
+  {
+    id: 9,
+    name: 'Tom Delonge',
+    email: 'tom@mail.com',
+    phone: '999-999-9999',
+    avatar: 'https://randomuser.me/api/portraits',
+    birthday: '1975-12-13',
+  },
+  {
+    id: 10,
+    name: 'Matt Skiba',
+    email: 'mark@mail.com',
+    phone: '101-010-1010',
+    avatar: 'https://randomuser.me/api/portraits',
+    birthday: '1976-01-24',
+  },
+];
+
+export default contacts;
---
+++ src/types/contact.d.ts
@@ -0,0 +1,8 @@
+export default interface Contact {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  avatar: string;
+  birthday: string;
+}
