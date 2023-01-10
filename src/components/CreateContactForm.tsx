2780eeeb4530ecbd3260b6ca37a5d641f68ab1c0
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-01-10 15:52:16 -0500

    Add: State to form

--- src/components/CreateContactForm.tsx
+++ src/components/CreateContactForm.tsx
@@ -1,15 +1,43 @@
+import { useState } from 'react';
+
 function CreateContactForm() {
+  const [data, setData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    avatar: '',
+    gender: '',
+    birthday: '',
+  });
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = event.target;
+
+    setData({ ...data, [name]: value });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log(data);
+  };
+
   return (
-    <form className='flex flex-col gap-2 mt-2'>
+    <form className='flex flex-col gap-2 mt-2' onSubmit={handleSubmit}>
       <div className='flex flex-col gap-1'>
         <label htmlFor='name' className='text-gray-800'>
           Full Name:
         </label>
         <input
           type='text'
+          name='name'
           id='name'
           className='p-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500'
           placeholder='John Doe'
+          value={data.name}
+          onChange={handleChange}
         />
       </div>
       <div className='flex flex-col gap-1'>
@@ -18,9 +46,12 @@ function CreateContactForm() {
         </label>
         <input
           type='text'
+          name='email'
           id='email'
           className='p-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500'
           placeholder='john@example.com'
+          value={data.email}
+          onChange={handleChange}
         />
       </div>
       <div className='flex flex-col gap-1'>
@@ -28,10 +59,13 @@ function CreateContactForm() {
           Phone:
         </label>
         <input
-          type='number'
+          type='text'
+          name='phone'
           id='name'
           className='p-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500'
           placeholder='555-555-555'
+          value={data.phone}
+          onChange={handleChange}
         />
       </div>
       <div className='flex flex-col gap-1'>
@@ -40,8 +74,11 @@ function CreateContactForm() {
         </label>
         <input
           type='file'
+          name='avatar'
           id='avatar'
           className='p-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500'
+          value={data.avatar}
+          onChange={handleChange}
         />
       </div>
       <div className='flex flex-col gap-1'>
@@ -50,15 +87,23 @@ function CreateContactForm() {
         </label>
         <input
           type='date'
+          name='birthday'
           id='birthday'
           className='p-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500'
+          value={data.birthday}
+          onChange={handleChange}
         />
       </div>
       <div className='flex flex-col gap-1'>
         <label htmlFor='gender' className='text-gray-800'>
           Gender:
         </label>
-        <select className='p-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500'>
+        <select
+          className='p-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500'
+          name='gender'
+          value={data.gender}
+          onChange={handleChange}
+        >
           <option defaultChecked>-- Select your gender --</option>
           <option value='male'>Male</option>
           <option value='female'>Female</option>
